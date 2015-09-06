53913f825809aaaa6209cb375c981b2ba0f347d3
Nina Satragno <nsatragno@gmail.com>
2015-09-06 19:54:09 -0300

    Commit inicial.

---
+++ src/com/gfive/tateti/interfaz/Menu.java
@@ -0,0 +1,37 @@
+package com.gfive.tateti.interfaz;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ * Barra de menú mostrada en la parte superior derecha de la pantalla.
+ * 
+ * @author nicolas
+ *
+ */
+public class Menu extends JMenuBar {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construye un menú, creando todos los items adecuados.
+	 */
+	public Menu() {
+		JMenu archivo = new JMenu("Archivo");
+		add(archivo);
+
+		JMenuItem abrir = new JMenuItem("Abrir...");
+		abrir.addActionListener((action) -> System.out.println("Abriendo"));
+		archivo.add(abrir);
+
+		JMenuItem abrirCarpeta = new JMenuItem("Abrir carpeta...");
+		abrirCarpeta.addActionListener((action) -> System.out
+				.println("Abriendo carpeta"));
+		archivo.add(abrirCarpeta);
+
+		JMenuItem salir = new JMenuItem("Salir");
+		salir.addActionListener((action) -> System.out.println("Saliendo"));
+		archivo.add(salir);
+	}
+}
---
+++ src/com/gfive/tateti/interfaz/VentanaPrincipal.java
@@ -0,0 +1,51 @@
+package com.gfive.tateti.interfaz;
+
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTree;
+
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * Ventana principal de la aplicación, conteniendo el panel de código y las métricas.
+ * @author nicolas
+ *
+ */
+public class VentanaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * El nombre de la aplicación mostrado al usuario.
+	 */
+	private static final String NOMBRE_APLICACION = "tateti";
+
+	/**
+	 * Arranca la aplicación.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				VentanaPrincipal ventana = new VentanaPrincipal();
+				ventana.setVisible(true);
+			}
+		});
+	}
+
+	/**
+	 * Inicializa todos los widgets de la ventana principal.
+	 */
+	public VentanaPrincipal() {
+		setTitle(NOMBRE_APLICACION);
+		setBounds(0, 0, 1024, 768);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setJMenuBar(new Menu());
+		setLayout(new MigLayout());
+		
+		Container pane = getContentPane();
+		JTree arbol = new JTree();
+		pane.add(arbol);
+	}
+}
