421c42c06f05500233b1e3c1ff9001c8f58be2ff
Nina Satragno <nsatragno@gmail.com>
2015-09-08 19:06:48 -0300

    Mejorada UI.

--- src/com/gfive/tateti/interfaz/VentanaPrincipal.java
+++ src/com/gfive/tateti/interfaz/VentanaPrincipal.java
@@ -6,17 +6,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTree;
+
+import net.miginfocom.swing.MigLayout;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 import com.gfive.tateti.componentes.arbol.ArbolArchivos;
-import com.gfive.tateti.exception.TatetiException;
-
-import net.miginfocom.swing.MigLayout;
 
 /**
  * Ventana principal de la aplicación, conteniendo el panel de código y las métricas.
@@ -55,18 +52,19 @@ public class VentanaPrincipal extends JFrame {
         setBounds(0, 0, 1024, 768);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setJMenuBar(new Menu());
-        setLayout(new MigLayout("", "[grow][grow]", "[grow]"));
+        setLayout(new MigLayout("debug", "[][grow]", "[grow]"));
+        //setLayout(new MigLayout("debug", "", ""));
 
         Container pane = getContentPane();
 
         ArbolArchivos arbol = new ArbolArchivos();
         arbol.cargarNodos(RUTA_INICIAL);
-        pane.add(arbol, "growy, growx");
+        pane.add(arbol, "width 30%, growy");
 
-        RSyntaxTextArea textArea = new RSyntaxTextArea(/*20, 60*/);
+        RSyntaxTextArea textArea = new RSyntaxTextArea();
         textArea.setEditable(false);
         textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         RTextScrollPane scrollPane = new RTextScrollPane(textArea);
-        pane.add(scrollPane, "growx, growy, wrap");
+        pane.add(scrollPane, "grow, wrap");
     }
 }
