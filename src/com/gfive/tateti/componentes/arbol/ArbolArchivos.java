47c4c4627b6a4dcb1a58b5cb5edecc05a0414b28
Nina Satragno <nsatragno@gmail.com>
2015-09-12 17:52:26 -0300

    Actualizados javadocs.

--- src/com/gfive/tateti/componentes/arbol/ArbolArchivos.java
+++ src/com/gfive/tateti/componentes/arbol/ArbolArchivos.java
@@ -28,8 +28,8 @@ public class ArbolArchivos extends JTree {
     }
 
     /**
-     * Carga recursivamente en el árbol la estructura del sistema de archivos a partir del path
-     * dado.
+     * Carga recursivamente en el árbol la estructura del sistema de archivos, actualizando la
+     * vista.
      * 
      * @param rutaInicial
      *            - la ruta donde se empieza a examinar el sistema de archivos.
@@ -39,7 +39,7 @@ public class ArbolArchivos extends JTree {
         setRootVisible(true);
         getRaiz().removeAllChildren();
         
-        Map<Path, Path> archivosQueLlenar = new ConcurrentHashMap<Path, Path>();
+        ConcurrentHashMap<Path, Path> archivosQueLlenar = new ConcurrentHashMap<Path, Path>();
         marcarArchivosParaCargar(rutaInicial, archivosQueLlenar);
 
         llenarArbol(getRaiz(), rutaInicial, archivosQueLlenar);
@@ -54,25 +54,35 @@ public class ArbolArchivos extends JTree {
     }
     
     /**
-     * Actualiza el conjunto de todos los archivos que deberían cargarse en el árbol de la
-     * aplicación.
+     * Recorre el árbol de archivos a partir del archivo dado. Si el archivo es .java o es una
+     * carpeta con archivos .java en alguna parte de su jerarquía, la agrega al conjunto pasado.
      * 
-     * @param archivo
-     * @param conjunto
+     * @param archivo - archivo del que se parte para marcar los archivos.
+     * @param conjunto - mapa en el que se van guardando los archivos que se marcan para inserción
+     *                   en el árbol.
+     * @return true si agregó el archivo pasado al árbol, false de lo contrario.
      */
-    private boolean marcarArchivosParaCargar(Path archivo, Map<Path, Path> conjunto) {
+    private boolean marcarArchivosParaCargar(Path archivo, ConcurrentHashMap<Path, Path> conjunto) {
         if (Files.isRegularFile(archivo) &&
             archivo.getFileName().toString().toLowerCase().endsWith(".java")) {
+            // Si es un archivo normal y .java, lo agregamos al conjunto y ya terminamos.
             conjunto.put(archivo, archivo);
             return true;
         }
-        if (!Files.isDirectory(archivo))
+        if (!Files.isDirectory(archivo)) {
+            // El archivo no es una carpeta, y no es .java. No nos interesa.
             return false;
+        }
 
         try {
+            // Recorremos todos los hijos, llamando recursivamente a esta función. Si alguno
+            // se agrega, agregamos a este archivo también.
             boolean marcada =  Files.list(archivo)
                  .parallel()
                  .map((hijo) -> marcarArchivosParaCargar(hijo, conjunto))
+                 // La reducción se asegura que, con que uno devuelva true, el resultado sea true.
+                 // Evitamos usar anyMatch() para asegurarnos que se recorran todos los elementos
+                 // del stream.
                  .reduce(false, (r1, r2) -> r1 == true || r2 == true);
             if (marcada)
                 conjunto.put(archivo, archivo);
@@ -83,19 +93,20 @@ public class ArbolArchivos extends JTree {
     }
 
     /**
-     * Recorre el sistema de archivos, buscando archivos .java y carpetas para agregar al árbol,
-     * hijos de la ruta pasada.
+     * Recorre el sistema de archivos, buscando archivos para agregar al árbol, hijos de la ruta
+     * pasada contenidos el archivosQueLlenar.
      * 
      * @param predecesor
      *            - nodo padre del subárbol que se creará.
-     * @param rutaNueva
+     * @param archivo
      *            - archivo del que se parte para llenar el árbol.
-     * @param archivosQueLlenar 
+     * @param archivosQueLlenar
+     *            - mapa con los archivos que deben insertarse en el árbol.
      */
     private void llenarArbol(DefaultMutableTreeNode predecesor,
-                             Path rutaNueva,
+                             Path archivo,
                              Map<Path, Path> archivosQueLlenar) {
-        NodoArbol nodo = NodoArbol.construir(rutaNueva);
+        NodoArbol nodo = NodoArbol.construir(archivo);
         predecesor.add(nodo);
 
         if (!nodo.esCarpeta())
